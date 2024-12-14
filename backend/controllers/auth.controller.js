2a9721e0f01411d58497b9dee73d8eeb9a942eee
Santii05-C <santi.c.pgm@gmail.com>
2024-12-13 22:08:04 -0300

    last commit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,31 @@
+import User from "../models/user.model.js";
+
 export const signup = async (req, res) => {
-  res.send("Sign up route called");
+  const { email, password, name } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const user = await User.create({ name, email, password });
+
+    // authenticate
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+
+    setCookies(res, accessToken, refreshToken);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ message: error.message });
+  }
 };
 export const login = async (req, res) => {
   res.send("login route called");
