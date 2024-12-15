b67b825f487169a437dcb88903d963b708764475
Santii05-C <santi.c.pgm@gmail.com>
2024-12-14 22:56:28 -0300

    last commit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { redis } from "../lib/redis.js";
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
@@ -12,7 +13,16 @@ const generateTokens = (userId) => {
   return { accessToken, refreshToken };
 };
 
-//49:50
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refresh_token:${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60
+  );
+};
+
+//52:48
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -25,6 +35,9 @@ export const signup = async (req, res) => {
     const user = await User.create({ name, email, password });
 
     const { accessToken, refreshToken } = generateTokens(user_.id);
+    await storeRefreshToken(user._id, refreshToken);
+
+    setCookies(res, accessToken, refreshToken);
 
     res.status(201).json({ user, message: "User created successfully" });
   } catch (error) {
