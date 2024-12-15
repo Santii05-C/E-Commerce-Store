aaffc5c0a854dac34cacdd7a6098b0080433de09
Santii05-C <santi.c.pgm@gmail.com>
2024-12-14 23:12:57 -0300

    last commit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,50 +1,50 @@
-import { redis } from "../lib/redis.js";
-import User from "../models/user.model.js";
-import jwt from "jsonwebtoken";
+// import { redis } from "../lib/redis.js";
+// import User from "../models/user.model.js";
+// import jwt from "jsonwebtoken";
 
-const generateTokens = (userId) => {
-  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "15m",
-  });
+// const generateTokens = (userId) => {
+//   const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+//     expiresIn: "15m",
+//   });
 
-  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn: "7d",
-  });
-  return { accessToken, refreshToken };
-};
+//   const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+//     expiresIn: "7d",
+//   });
+//   return { accessToken, refreshToken };
+// };
 
-const storeRefreshToken = async (userId, refreshToken) => {
-  await redis.set(
-    `refresh_token:${userId}`,
-    refreshToken,
-    "EX",
-    7 * 24 * 60 * 60
-  );
-};
+// const storeRefreshToken = async (userId, refreshToken) => {
+//   await redis.set(
+//     `refresh_token:${userId}`,
+//     refreshToken,
+//     "EX",
+//     7 * 24 * 60 * 60
+//   );
+// };
 
-//52:48
+// //52:48
 
-export const signup = async (req, res) => {
-  const { email, password, name } = req.body;
-  try {
-    const userExists = await User.findOne({ email });
+// export const signup = async (req, res) => {
+//   const { email, password, name } = req.body;
+//   try {
+//     const userExists = await User.findOne({ email });
 
-    if (userExists) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-    const user = await User.create({ name, email, password });
+//     if (userExists) {
+//       return res.status(400).json({ message: "User already exists" });
+//     }
+//     const user = await User.create({ name, email, password });
 
-    const { accessToken, refreshToken } = generateTokens(user_.id);
-    await storeRefreshToken(user._id, refreshToken);
+//     const { accessToken, refreshToken } = generateTokens(user_.id);
+//     await storeRefreshToken(user._id, refreshToken);
 
-    setCookies(res, accessToken, refreshToken);
+//     setCookies(res, accessToken, refreshToken);
 
-    res.status(201).json({ user, message: "User created successfully" });
-  } catch (error) {
-    console.log("Error in signup controller", error.message);
-    res.status(500).json({ message: error.message });
-  }
-};
+//     res.status(201).json({ user, message: "User created successfully" });
+//   } catch (error) {
+//     console.log("Error in signup controller", error.message);
+//     res.status(500).json({ message: error.message });
+//   }
+// };
 
-export const login = async (req, res) => {};
-export const logout = async (req, res) => {};
+// export const login = async (req, res) => {};
+// export const logout = async (req, res) => {};
