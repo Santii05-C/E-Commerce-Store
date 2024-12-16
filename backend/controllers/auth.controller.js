0cfc36d4cb97c2e52a9cea7cbbeeb7bca623b9b7
Santii05-C <santi.c.pgm@gmail.com>
2024-12-15 23:06:26 -0300

    last commit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -22,7 +22,21 @@ const storeRefreshToken = async (userId, refreshToken) => {
   );
 };
 
-//52:48
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "Strict",
+    maxAge: 15 * 60 * 1000,
+  });
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "Strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+};
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -34,17 +48,22 @@ export const signup = async (req, res) => {
     }
     const user = await User.create({ name, email, password });
 
-    const { accessToken, refreshToken } = generateTokens(user_.id);
+    const { accessToken, refreshToken } = generateTokens(user._id);
     await storeRefreshToken(user._id, refreshToken);
 
     setCookies(res, accessToken, refreshToken);
 
-    res.status(201).json({ user, message: "User created successfully" });
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
   } catch (error) {
     console.log("Error in signup controller", error.message);
     res.status(500).json({ message: error.message });
   }
 };
-
+//56:31
 export const login = async (req, res) => {};
 export const logout = async (req, res) => {};
