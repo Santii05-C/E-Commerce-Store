75cce9af3781173a2a640a00d84471d0c819c2af
Santii05-C <santi.c.pgm@gmail.com>
2024-12-18 10:07:48 -0300

    last commit

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getAllProducts } from "../controllers/product.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", protectRouter, adminRoute, getAllProducts);
+router.get("/", protectRoute, adminRoute, getAllProducts);
 
 export default router;
