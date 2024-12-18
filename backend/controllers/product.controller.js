6955683d6f242b851447b9c59216ad65c3e9b923
Santii05-C <santi.c.pgm@gmail.com>
2024-12-18 10:57:06 -0300

    last commit

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import { redis } from "../lib/redis.js";
 import Product from "../models/product.model.js";
 
 export const getAllProducts = async (req, res) => {
@@ -7,5 +8,18 @@ export const getAllProducts = async (req, res) => {
   } catch (error) {
     console.log("Error in getAllProducts controller " + error.message);
     res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  let featuredProducts = await redis.get("featured_products");
+  if (featuredProducts) {
+    return res.json(JSON.parse(featuredProducts));
+  }
+
+  featuredProducts = await Product.redis.fins({ isFeatured: true }).lean();
+
+  if (!featuredProducts) {
+    return res.status(404).json({ message: "No featured products found" });
   }
 };
