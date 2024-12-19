5bcc1c193fe308e272b462359563071557cc0ee0
Santii05-C <santi.c.pgm@gmail.com>
2024-12-19 15:26:59 -0300

    last commit

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -6,6 +6,7 @@ import {
   deleteProduct,
   getRecommendedProducts,
   getProductsByCategory,
+  toggleFeaturedProduct,
 } from "../controllers/product.controller.js";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
@@ -16,6 +17,7 @@ router.get("/featured", getFeaturedProducts);
 router.get("/category/:category", getProductsByCategory);
 router.get("/recommendations", getRecommendedProducts);
 router.post("/", protectRoute, adminRoute, createProduct);
+router.post("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
 router.post("/:id", protectRoute, adminRoute, deleteProduct);
 
 export default router;
