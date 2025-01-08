0b193f0589f97ffe41b9d84006b9ded4bbd2586a
Santii05-C <santi.c.pgm@gmail.com>
2025-01-07 21:16:34 -0300

    last commit

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -26,3 +26,50 @@ export const getAnalyticData = async () => {
     totalRevenue,
   };
 };
+
+export const getDeilySalesDate = async (startDate, endDate) => {
+  const dailySalesData = await Order.aggregate([
+    {
+      $match: {
+        createdAt: {
+          $gte: startDate,
+          $lte: endDate,
+        },
+      },
+    },
+    {
+      $group: {
+        _id: { $dateToString: { format: "%Y-%m-%d", date: "%createdAt" } },
+        sales: { $sum: 1 },
+        revenue: { $sum: "$totalAmount" },
+      },
+    },
+    { $sort: { _id: 1 } },
+  ]);
+
+  const dateArray = getDatesInRange(startDate, endDate);
+
+  return dateArray.map((date) => {
+    const foundDate = dailySalesData.find((item) => item._id === date);
+
+    return {
+      date,
+      sales: foundDate?.sales || 0,
+      revenue: foundDate?.revenue || 0,
+    };
+  });
+
+  re;
+};
+
+function getDatesInRange(startDate, endDate) {
+  const dates = [];
+  let currentDate = new Date(startDate);
+
+  while (currentDate <= endDate) {
+    dates.push(currentDate.toISOString().split("T")[0]);
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return dates;
+}
