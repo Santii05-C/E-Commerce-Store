0927f567366f9abd5d3a69adf8bc4dfeab8fb26c
Santii05-C <santi.c.pgm@gmail.com>
2025-01-08 22:40:18 -0300

    last commit

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -1,30 +1,30 @@
-import express from "express";
-import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
-import {
-  getAnalyticData,
-  getDailySalesData,
-} from "../controllers/analytics.controller.js";
+// import express from "express";
+// import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
+// import {
+//   getAnalyticData,
+//   getDailySalesData,
+// } from "../controllers/analytics.controller.js";
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get("/", protectRoute, adminRoute, async (req, res) => {
-  try {
-    const analyticsData = await getAnalyticData();
+// router.get("/", protectRoute, adminRoute, async (req, res) => {
+//   try {
+//     const analyticsData = await getAnalyticData();
 
-    const endDate = new Date();
-    const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+//     const endDate = new Date();
+//     const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000);
 
-    const dailySalesData = await getDailySalesData(startDate, endDate);
+//     const dailySalesData = await getDailySalesData(startDate, endDate);
 
-    res.json({
-      analyticsData,
-      dailySalesData,
-    });
-  } catch (error) {
-    console.log("Error in analytics route", error.message);
-    res.status(500).json({ message: "Server error", error: error.message });
-  }
-});
+//     res.json({
+//       analyticsData,
+//       dailySalesData,
+//     });
+//   } catch (error) {
+//     console.log("Error in analytics route", error.message);
+//     res.status(500).json({ message: "Server error", error: error.message });
+//   }
+// });
 
-export default router;
-//3:12
+// export default router;
+// //3:12
