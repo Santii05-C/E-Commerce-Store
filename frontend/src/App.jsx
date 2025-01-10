f01e2db1bf1707b209ed6a387b5c2afbcd8e88e2
Santii05-C <santi.c.pgm@gmail.com>
2025-01-09 21:51:41 -0300

    last commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
+import { Router, Route } from "express";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
-  return <div className="text-red-500 text-5xl">hello world</div>;
+  return (
+    <Router>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Router>
+  );
 }
 
 export default App;
