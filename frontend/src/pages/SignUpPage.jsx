ec3371a5bb7fdf369d4367c6759700c48600f5ad
Santii05-C <santi.c.pgm@gmail.com>
2025-01-16 22:45:38 -0300

    last commit

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
+import { useUserStore } from "../stores/useUserStore";
 
 const SignUpPage = () => {
   const loading = false;
@@ -13,11 +14,13 @@ const SignUpPage = () => {
     confirmPassword: "",
   });
 
+  const { signup, user } = useUserStore();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formData);
   };
-  //3:38
+
   return (
     <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <motion.div
--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -1,1 +1,11 @@
-//4:10
+import { create } from "zustand";
+import axios from "../lib/axios";
+import { toast } from "react-hot-toast";
+
+export const useUserStore = create((set, get) => ({
+  user: null,
+  loading: false,
+  checkingAuth: true,
+
+  signup: async () => {},
+}));
