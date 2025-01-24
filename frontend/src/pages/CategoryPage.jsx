db75f9377ff6310f7d1a594c067a43013d93c2d7
Santii05-C <santi.c.pgm@gmail.com>
2025-01-23 23:18:30 -0300

    last commit

--- frontend/src/pages/CategoryPage.jsx
+++ frontend/src/pages/CategoryPage.jsx
@@ -1,16 +1,16 @@
-import { useEffect } from "react";
-import { useProductStore } from "../stores/useProductStore";
-import { useParams } from "react-router-dom";
-const CategoryPage = () => {
-  const { fetchProductsByCategory, products } = useProductStore();
+// import { useEffect } from "react";
+// import { useProductStore } from "../stores/useProductStore";
+// import { useParams } from "react-router-dom";
+// const CategoryPage = () => {
+//   const { fetchProductsByCategory, products } = useProductStore();
 
-  const { category } = useParams();
-  //5:06
-  useEffect(() => {
-    fetchProductsByCategory("shoes");
-  }, [fetchProductsByCategory]);
+//   const { category } = useParams();
+//   //5:06
+//   useEffect(() => {
+//     fetchProductsByCategory("shoes");
+//   }, [fetchProductsByCategory]);
 
-  console.log("products:", products);
-  return <div className="">CategoryPage</div>;
-};
-export default CategoryPage;
+//   console.log("products:", products);
+//   return <div className="">CategoryPage</div>;
+// };
+// export default CategoryPage;
