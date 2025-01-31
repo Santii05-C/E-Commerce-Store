f0195dc4b29298f087f91a625fd0c5779164e75c
Santii05-C <santi.c.pgm@gmail.com>
2025-01-30 22:36:06 -0300

    last commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import { useEffect } from "react";
 import LoadingSpinner from "./components/LoadingSpinner";
 import CartPage from "./pages/CartPage";
 import PurchaseSuccessPage from "./pages/PurchaseSuccessPage";
+import PurchaseCancelPage from "./pages/PurchaseCancelPage";
 
 function App() {
   const { user, checkAuth, checkingAuth } = useUserStore();
@@ -63,6 +64,10 @@ function App() {
           <Route
             path="/purchase-success"
             element={user ? <PurchaseSuccessPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/purchase-cancel"
+            element={user ? <PurchaseCancelPage /> : <Navigate to="/login" />}
           />
         </Routes>
       </div>
