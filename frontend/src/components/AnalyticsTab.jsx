b72a628e0981be66bf66e1f112505fd8aadaf1a6
Santii05-C <santi.c.pgm@gmail.com>
2025-02-01 11:06:48 -0300

    last commit

--- frontend/src/components/AnalyticsTab.jsx
+++ frontend/src/components/AnalyticsTab.jsx
@@ -1,5 +1,53 @@
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
 const AnalyticsTab = () => {
+  const [analyticsData, setAnalyticsData] = useState({
+    users: 0,
+    products: 0,
+    totalSales: 0,
+    totalRevenue: 0,
+  });
+  const [isLoading, setIsLoading] = useState(true);
+  const [dailySalesData, setDailySalesData] = useState([]);
+
+  useEffect(() => {
+    const fetchAnalyticsData = async () => {
+      try {
+        const response = await axios.get("/analytics");
+        setAnalyticsData(response.data.analyticsData);
+        setDailySalesData(response.data.dailySalesData);
+      } catch (error) {
+        console.error("Error fetching analytics data:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchAnalyticsData();
+  }, []);
+
   return <div>AnalyticsTab</div>;
 };
 
 export default AnalyticsTab;
+
+const AnalyticsCard = ({ title, value, icon: Icon, color }) => (
+  <motion.div
+    className={`bg-gray-800 rounded-lg p-6 shadow-lg overflow-hidden relative ${color}`}
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.5 }}
+  >
+    <div className="flex justify-between items-center">
+      <div className="z-10">
+        <p className="text-emerald-300 text-sm mb-1 font-semibold">{title}</p>
+        <h3 className="text-white text-3xl font-bold">{value}</h3>
+      </div>
+    </div>
+    <div className="absolute inset-0 bg-gradient-to-br from-emerald-600 to-emerald-900 opacity-30" />
+    <div className="absolute -bottom-4 -right-4 text-emerald-800 opacity-50">
+      <Icon className="h-32 w-32" />
+    </div>
+  </motion.div>
+);
