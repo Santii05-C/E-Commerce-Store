8d8619d278248d3be8270641416d0c958fc85067
Santii05-C <santi.c.pgm@gmail.com>
2025-02-04 22:47:14 -0300

    last commit

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -1,91 +1,91 @@
-import { create } from "zustand";
-import axios from "../lib/axios";
-import { toast } from "react-hot-toast";
+// import { create } from "zustand";
+// import axios from "../lib/axios";
+// import { toast } from "react-hot-toast";
 
-export const useUserStore = create((set, get) => ({
-  user: null,
-  loading: false,
-  checkingAuth: true,
+// export const useUserStore = create((set, get) => ({
+//   user: null,
+//   loading: false,
+//   checkingAuth: true,
 
-  signup: async ({ name, email, password, confirmPassword }) => {
-    set({ loading: true });
+//   signup: async ({ name, email, password, confirmPassword }) => {
+//     set({ loading: true });
 
-    if (password !== confirmPassword) {
-      set({ loading: false });
-      return toast.error("Passwords do not match");
-    }
+//     if (password !== confirmPassword) {
+//       set({ loading: false });
+//       return toast.error("Passwords do not match");
+//     }
 
-    try {
-      const res = await axios.post("/auth/signup", { name, email, password });
-      set({ user: res.data, loading: false });
-    } catch (error) {
-      set({ loading: false });
-      toast.error(error.response.data.message || "An error occurred");
-    }
-  },
+//     try {
+//       const res = await axios.post("/auth/signup", { name, email, password });
+//       set({ user: res.data, loading: false });
+//     } catch (error) {
+//       set({ loading: false });
+//       toast.error(error.response.data.message || "An error occurred");
+//     }
+//   },
 
-  login: async (email, password) => {
-    set({ loading: true });
+//   login: async (email, password) => {
+//     set({ loading: true });
 
-    try {
-      const res = await axios.post("/auth/login", { email, password });
+//     try {
+//       const res = await axios.post("/auth/login", { email, password });
 
-      set({ user: res.data, loading: false });
-    } catch (error) {
-      set({ loading: false });
-      toast.error(error.response.data.message || "An error occurred");
-    }
-  },
+//       set({ user: res.data, loading: false });
+//     } catch (error) {
+//       set({ loading: false });
+//       toast.error(error.response.data.message || "An error occurred");
+//     }
+//   },
 
-  logout: async () => {
-    try {
-      await axios.post("/auth/logout");
-      set({ user: null });
-    } catch (error) {
-      toast.error(
-        error.response?.data?.message || "An error occurred during logout"
-      );
-    }
-  },
+//   logout: async () => {
+//     try {
+//       await axios.post("/auth/logout");
+//       set({ user: null });
+//     } catch (error) {
+//       toast.error(
+//         error.response?.data?.message || "An error occurred during logout"
+//       );
+//     }
+//   },
 
-  checkAuth: async () => {
-    set({ checkingAuth: true });
-    try {
-      const response = await axios.get("/auth/profile");
-      set({ user: response.data, checkingAuth: false });
-    } catch (error) {
-      console.log(error.message);
-      set({ checkingAuth: false, user: null });
-    }
-  },
-}));
+//   checkAuth: async () => {
+//     set({ checkingAuth: true });
+//     try {
+//       const response = await axios.get("/auth/profile");
+//       set({ user: response.data, checkingAuth: false });
+//     } catch (error) {
+//       console.log(error.message);
+//       set({ checkingAuth: false, user: null });
+//     }
+//   },
+// }));
 
-let refreshPromise = null;
+// let refreshPromise = null;
 
-axios.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
+// axios.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const originalRequest = error.config;
+//     if (error.response?.status === 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
 
-      try {
-        if (refreshPromise) {
-          await refreshPromise;
-          return axios(originalRequest);
-        }
+//       try {
+//         if (refreshPromise) {
+//           await refreshPromise;
+//           return axios(originalRequest);
+//         }
 
-        refreshPromise = useUserStore.getState().refreshToken();
-        await refreshPromise;
-        refreshPromise = null;
+//         refreshPromise = useUserStore.getState().refreshToken();
+//         await refreshPromise;
+//         refreshPromise = null;
 
-        return axios(originalRequest);
-      } catch (refreshError) {
-        useUserStore.getState().logout();
-        return Promise.reject(refreshError);
-      }
-    }
-    return Promise.reject(error);
-  }
-);
-//7:17
+//         return axios(originalRequest);
+//       } catch (refreshError) {
+//         useUserStore.getState().logout();
+//         return Promise.reject(refreshError);
+//       }
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+// //7:17
